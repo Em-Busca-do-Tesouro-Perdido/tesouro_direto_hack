964690c0d8c9a0b793726a3a0786356da56e9efd
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2023-11-27 09:47:17 -0300

    feature: Cria contratos para doações orçamentárias

---
+++ contracts/CriarDoacaoOrcamentaria.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import {DoacoesOrcamentarias} from "./DoacoesOrcamentarias.sol";
+
+contract TransferenciasEspeciais is Ownable {
+    
+
+    address[] public doacoesOrcamentariasContracts; 
+
+    event DotacaoOrcamentariaCriada(address indexed owner, address indexed contrato);
+
+    function criarDotacaoOrcamentaria(string memory name, string memory symbol) public onlyOwner {
+        DoacoesOrcamentarias dotacaoOrcamentaria = new DoacoesOrcamentarias(name, symbol);
+        doacoesOrcamentariasContracts.push(address(dotacaoOrcamentaria));
+        emit DotacaoOrcamentariaCriada(msg.sender, address(dotacaoOrcamentaria));
+    }
+
+    function getQuantidadeContratosDoacoesOrcamentarias() public view returns (uint256) {
+        return doacoesOrcamentariasContracts.length;
+    }
+}
