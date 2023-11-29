170c9b116f298e95af7c42b9813724d2e925ee2f
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2023-11-29 15:05:34 -0300

    feature: Adiciona configurações da rede Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -9,6 +12,13 @@ const config: HardhatUserConfig = {
       }
     ]
   },
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_ENDPOINT,
+      accounts: [ process.env.PRIVATE_KEY ?? ""]
+    }
+  }
 };
 
 export default config;
