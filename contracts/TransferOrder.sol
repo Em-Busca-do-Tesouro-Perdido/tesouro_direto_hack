00218639698c08a6bdb2ca5a4ae36de796efadbe
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2023-11-30 12:45:26 -0300

    feature: Adiciona métodos para adicionar nova requisição de transferência e editar campo de transação efetuada de uma já existente

--- contracts/TransferOrder.sol
+++ contracts/TransferOrder.sol
@@ -4,15 +4,25 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TransferOrder {
 
+    struct SpecialTransferRequest {
+        string receiver;
+        string parlamentRequest;
+        string eligibleDocument;
+        uint256 value;
+        string effectedTransaction;
+    }
+
+    IERC20 token;
+
     uint256 balance;
     address treasuryPublicKey;
-    IERC20 token;    
+    SpecialTransferRequest[] specialTransferRequests;
+
     constructor(uint256 _balance, address _token, address _treasuryPublicKey) {
         balance = _balance;
         token = IERC20(_token);
         treasuryPublicKey = _treasuryPublicKey;
     }
-
  
     function getBalance() public view returns (uint256) {
         return balance;
@@ -25,5 +35,21 @@ contract TransferOrder {
     function specialTransfer(uint256 _amount, address _receiver) public {
         require(_amount <= balance);
         token.transferFrom(treasuryPublicKey, _receiver, _amount);
+    }
+
+    function addSpecialTransferRequest(string memory _receiver, string memory _parlamentRequest, string memory _eligibleDocument, uint256 _value) public {
+        specialTransferRequests.push(
+            SpecialTransferRequest({
+                eligibleDocument: _eligibleDocument,
+                receiver: _receiver,
+                parlamentRequest: _parlamentRequest,
+                value: _value,
+                effectedTransaction: ""
+            })
+        );
+    }
+
+    function updateEffectedTransaction(uint256 _index, string memory _effectedTransaction) public {
+        specialTransferRequests[_index].effectedTransaction = _effectedTransaction;
     }
 }
