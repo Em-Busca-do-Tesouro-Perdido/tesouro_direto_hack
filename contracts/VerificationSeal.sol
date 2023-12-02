4e3501882dc37bc741f5355ec850c4c6107ddef9
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2023-12-02 11:39:07 -0300

    refactor: Permite apenas que endereços autorizados consigam mintar o selo

--- contracts/VerificationSeal.sol
+++ contracts/VerificationSeal.sol
@@ -5,12 +5,25 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VerificationSeal is ERC721, Ownable {
-    constructor(address initialOwner)
+    constructor(address _initialOwner)
         ERC721("VerificationSeal", "VRS")
-        Ownable(initialOwner)
-    {}
+        Ownable(_initialOwner)
+    {
+        authorizedTcAddresses[_initialOwner] = true;
+    }
+    
+    mapping (address=>bool) authorizedTcAddresses;
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
+    modifier onlyTcAddressess {
+        require(authorizedTcAddresses[msg.sender] == true);
+        _;
+    }
+
+    function addTcAllowedAddress(address _tcAddress) public onlyOwner {
+        authorizedTcAddresses[_tcAddress] = true;
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyTcAddressess() {
         _safeMint(to, tokenId);
     }
 }
