da3bb7b143d54b5339d50c82a4db50394af77f5a
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2023-12-02 11:13:36 -0300

    fix: Conserta NFT para ser mintável

--- contracts/VerificationSeal.sol
+++ contracts/VerificationSeal.sol
@@ -2,7 +2,15 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VerificationSeal is ERC721 {
-    constructor() ERC721("VerificationSeal", "VRS") {}
+contract VerificationSeal is ERC721, Ownable {
+    constructor(address initialOwner)
+        ERC721("VerificationSeal", "VRS")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
 }
