3232b5d3eccfc4193c9e07ff9f275e2f21e8f11f
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2023-12-04 10:31:57 -0300

    feature: Cria scripts de chamada de métodos de escrita

---
+++ scripts/user_journey/balance_of.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function main() {
+    const treasuryAddress: string = process.env.TREASURY_PUBLIC_ADDRESS ?? "";
+    const erc20Contract = await ethers.getContractAt("HackToken", process.env.ERC20_CONTRACT_ADDRESS ?? "");
+
+    const balance = await erc20Contract.balanceOf(treasuryAddress ?? "");
+
+    console.log(`Balance of treasury account ${treasuryAddress} after mint operation: ${balance}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
